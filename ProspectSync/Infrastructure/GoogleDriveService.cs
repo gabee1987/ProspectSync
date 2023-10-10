a7a962c7f8f49111371120d95f797d901e30b668
gabee1987 <gabee1987@gmail.com>
2023-10-10 12:02:53 +0200

    created the basic alyout of the app with a test code to see the gdrive credentials are working

---
+++ ProspectSync/Infrastructure/GoogleDriveService.cs
@@ -0,0 +1,45 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ProspectSync.Infrastructure
+{
+    public class GoogleDriveService
+    {
+        private readonly DriveService _service;
+
+        public GoogleDriveService( string serviceAccountPath )
+        {
+            GoogleCredential credential;
+            using ( var stream = new FileStream( serviceAccountPath, FileMode.Open, FileAccess.Read ) )
+            {
+                credential = GoogleCredential.FromStream( stream )
+                    .CreateScoped( DriveService.Scope.Drive );
+            }
+
+            _service = new DriveService( new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "IcarusSaveShare",
+            } );
+        }
+
+        // Here you will add methods for operations like Upload, Download, ListFiles, etc.
+
+
+
+        // For test only
+        public IList<Google.Apis.Drive.v3.Data.File> ListFiles()
+        {
+            var request = _service.Files.List();
+            request.PageSize = 10; // List only 10 files for test purposes
+            request.Fields = "nextPageToken, files(id, name)";
+
+            var result = request.Execute();
+            return result.Files;
+        }
+    }
+}
---
+++ ProspectSync/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using ProspectSync.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProspectSync
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GoogleDriveService _driveService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Initialize the Drive service
+            _driveService = new GoogleDriveService( "credentials/icarussaveshare-82d0b173d4cf.json" );
+        }
+
+        private void CheckButton_Click( object sender, RoutedEventArgs e )
+        {
+            // Example logic for the Check button. You can add the logic you need.
+            var files = _driveService.ListFiles();
+            if ( files != null && files.Count > 0 )
+            {
+                MessageBox.Show( $"Found {files.Count} file(s)." );
+            }
+            else
+            {
+                MessageBox.Show( "No files found." );
+            }
+        }
+
+        private void GetSaveInfoButton_Click( object sender, RoutedEventArgs e )
+        {
+            // Add logic for getting save info
+        }
+
+        private void DownloadButton_Click( object sender, RoutedEventArgs e )
+        {
+            // Add logic for downloading the file
+        }
+
+        private void UploadButton_Click( object sender, RoutedEventArgs e )
+        {
+            // Add logic for uploading the file
+        }
+
+        private void BackupButton_Click( object sender, RoutedEventArgs e )
+        {
+            // Add logic for creating a backup
+        }
+    }
+}
